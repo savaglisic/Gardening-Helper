dd761049f1f7005ff51e2e09799b172b366f12eb
Sava Glisic <104223515+savaglisic@users.noreply.github.com>
2023-02-08 18:38:22 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var users = map[string]string{}
@@ -21,9 +22,26 @@ func main() {
 			}
 			username := r.Form.Get("username")
 			password := r.Form.Get("password")
-			print(username)
+
+			file, err := os.OpenFile("sample.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file.Close()
+
+			_, err = file.WriteString(username + ":" + password + "\n")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			print(username + ":")
 			print(password)
+			println("")
+
 			users[username] = password
+
 			fmt.Fprintf(w, "Login successful")
 		} else {
 			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
