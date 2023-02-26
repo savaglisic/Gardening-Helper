56aecf157e13d13e3af567c85105de03ef0cff57
scash1985 <s.cash@ufl.edu>
2023-02-25 23:12:12 -0500

    search bar and table added to My Garden

--- src/app/ourmission/ourmission.component.ts
+++ src/app/ourmission/ourmission.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ourmission',
   template: `
-    <p>
-      ourmission works!
+  <h1 class="title mx-5">Our Mission</h1>
+  <div class="container is-widescreen">
+   <div class="notification is-success">
+     <p class="is-centered mx-6">
+       Our mission is to provide gardeners with the tools and resources they need to plan, organize, and optimize their garden spaces. We believe that gardening is a fulfilling and important activity that benefits both individuals and communities. By helping gardeners stay organized and efficient, we hope to make the experience more enjoyable and rewarding for everyone involved. Our goal is to create a vibrant online community where gardeners can connect, learn from each other, and share their knowledge and experience.
     </p>
+   </div>
+  </div>
   `,
   styles: [
   ]
