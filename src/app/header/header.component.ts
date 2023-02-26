18a424426903469104034ff9a92b47f6a7df1461
scash1985 <s.cash@ufl.edu>
2023-02-25 23:12:39 -0500

    search and table added to My Garden

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,45 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" routerLink="/">
-    <img src="assets/Images/logo-black.png" width="50" height="200">
+    Home
     </a>
-
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      
-  
-      <a class="navbar-item" routerLink="/ourmission">
-        Our Mission
-      </a>
-
-      <a class="navbar-item" routerLink="/alerts">
-        Alerts
-      </a>
-
       <a class="navbar-item" routerLink="/mygarden">
         My Garden
       </a>
-
+      <a class="navbar-item" routerLink="/alerts">
+        Alerts
+      </a>
       <a class="navbar-item"  routerLink="/goodies">
         Gardening Goodies
       </a>
-
+      <a class="navbar-item" routerLink="/ourmission">
+        Our Mission
+      </a>
       <a class="navbar-item" routerLink="/contact">
         Contact Us
       </a>
-      
     </div>
-
     <div class="navbar-end">
       <div class="navbar-item" >
         <div class="buttons">
@@ -56,8 +46,6 @@ import { Component } from '@angular/core';
     </div>
   </div>
 </nav>
-
-  
   `,
   styles: [
   ]
--- src/app/mygarden/mygarden.component.ts
+++ src/app/mygarden/mygarden.component.ts
@@ -2,10 +2,57 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mygarden',
-  template: `
-    <p>
-      mygarden works!
-    </p>
+  template:  `
+  <h1 class="title mx-5">My Garden</h1>
+  <!-- <h2 class="subtitle">
+     Add Plants to My Garden
+  </h2> -->
+  <p class="mx-5">Search our plant database by name:</p>
+<section>
+<div class="field is-grouped mx-5 mb-5">
+  <p class="control">
+    <input class="input" type="text" placeholder="Find a repository">
+  </p>
+  <p class="control">
+    <a class="button is-success">
+      Search
+    </a>
+  </p>
+</div>
+<div>
+<table class="table is-fullwidth is-hoverable mx-5">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Temperature</th>
+      <th>PH</th>
+      <th>Soil</th>
+      <th>Waterlevel</th>
+      <th>Space</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Raddish</td>
+      <td>31</td>
+      <td>6.5</td>
+      <td>Well-drained Sandy Soil</td>
+      <td>High</td>
+      <td>0.025</td>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      <td>Beet</td>
+      <td>29</td>
+      <td>6.8</td>
+      <td>Well-drained Carbonic Soil</td>
+      <td>High</td>
+      <td>0.025</td>
+    </tr>
+  </tbody>
+</table>
+</div>
   `,
   styles: [
   ]
