a27cc1a9e96e3e9f391664afacc725c8bed41138
shaliniApatel03 <patels3@ufl.edu>
2023-03-01 22:00:24 -0500

    commit

--- main_test.go
+++ main_test.go
@@ -1,71 +1,94 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
-	"encoding/json"
-
-	//"io/ioutil"
-	//"io/outil"
 	"os"
-	//"reflect"
 	"strings"
 	"testing"
 )
 
-func TestVegetablePlant(t *testing.T) {
-	input := "Raddish\nq\n"
-	expectedOutput := "Enter a vegetable to search for (or 'q' to quit): Raddish\nName: Raddish\nIdeal Temperature for the plant: 31.0761254\nPH: 7.5\nSoil: Well-drained Sandy Soil\nWaterlevel: High \nSpace: 0.028\nEnter a vegetable to search for (or 'q' to quit): Sorry, the vegetable you entered was not found in the database.\n"
+func TestSearchPlant(t *testing.T) {
+	// Prepare test data
+	input := "rose"
+	expectedOutput := "Common Name: rose\nScientific Name: Rosa\nSlug: rosa\nRank: genus\nGenus:  Rosa\nFamily common name:  Rosaceae\nIs Vegetable: false"
 
-	// Set input buffer
-	inputBuf := strings.NewReader(input)
-	// Set output buffer
-	outputBuf := &bytes.Buffer{}
+	// Set up reader to simulate user input
+	reader := bufio.NewReader(strings.NewReader("yes\nrose\n"))
 
-	// Call vegetablePlant with the modified input and output streams
-	vegetablePlant(inputBuf, outputBuf)
-
-	if outputBuf.String() != expectedOutput {
-		t.Errorf("Unexpected output.\nExpected:\n%s\nActual:\n%s", expectedOutput, outputBuf.String())
-	}
-}
-
-func vegetablePlant(inputBuf *strings.Reader, outputBuf *bytes.Buffer) {
-	panic("unimplemented")
-}
-
-func TestRetrieveMyPlants(t *testing.T) {
-	// Create a test JSON file
-	testPlants := []Plant{
-		{CommonName: "Rose", ScientificName: "Rosa", Genus: "Rosaceae", Rank: "species"},
-		{CommonName: "Tulip", ScientificName: "Tulipa", Genus: "Liliaceae", Rank: "genus"},
-		{CommonName: "Lavender", ScientificName: "Lavandula", Genus: "Lamiaceae", Rank: "genus"},
-	}
-	testFile, err := os.CreateTemp("", "test-plants-*.json")
-	if err != nil {
-		t.Fatalf("Failed to create test file: %v", err)
-	}
-	defer os.Remove(testFile.Name())
-	encoder := json.NewEncoder(testFile)
-	for _, plant := range testPlants {
-		if err := encoder.Encode(plant); err != nil {
-			t.Fatalf("Failed to encode test data: %v", err)
-		}
-	}
-	testFile.Close()
+	// Replace stdout with a buffer to capture output
+	stdout := &bytes.Buffer{}
+	oldStdout := os.Stdout
+	os.Stdout = stdout
+	defer func() { os.Stdout = oldStdout }()
 
 	// Call the function being tested
-	plants, err := retrieveMyPlants()
-	if err != nil {
-		t.Fatalf("Failed to retrieve plants: %v", err)
+	searchPlant(reader, input)
+
+	// Compare actual output with expected output
+	if !strings.Contains(stdout.String(), expectedOutput) {
+		t.Errorf("searchPlant() output = %q; expected %q", stdout.String(), expectedOutput)
+	}
+}
+
+func TestAddPlant(t *testing.T) {
+	// Prepare test data
+	input := "rose\nA beautiful flower.\n.\n"
+	expectedOutput := "Plant added to the garden successfully!"
+
+	// Set up reader to simulate user input
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	// Replace stdout with a buffer to capture output
+	stdout := &bytes.Buffer{}
+	oldStdout := os.Stdout
+	os.Stdout = stdout
+	defer func() { os.Stdout = oldStdout }()
+
+	// Call the function being tested
+	addPlant(reader)
+
+	// Compare actual output with expected output
+	if !strings.Contains(stdout.String(), expectedOutput) {
+		t.Errorf("addPlant() output = %q; expected %q", stdout.String(), expectedOutput)
+	}
+}
+
+func TestEditPlant(t *testing.T) {
+	// Prepare test data
+	input := "rose\nA beautiful flower.\n.\n"
+	plantName := "rose"
+	expectedOutput := "Plant description updated successfully!"
+
+	// Set up reader to simulate user input
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	// Replace stdout with a buffer to capture output
+	stdout := &bytes.Buffer{}
+	oldStdout := os.Stdout
+	//os.Stdout = stdout
+	defer func() { os.Stdout = oldStdout }()
+
+	// Add plant to garden to prepare for editing
+	addPlant()
+
+	// Reset the buffer and reader
+	stdout.Reset()
+	reader.Reset(strings.NewReader("A beautiful and fragrant flower.\n.\n"))
+
+	// Call the function being tested
+	editPlant(reader)
+
+	// Compare actual output with expected output
+	if !strings.Contains(stdout.String(), expectedOutput) {
+		t.Errorf("editPlant() output = %q; expected %q", stdout.String(), expectedOutput)
 	}
 
-	// Assert that the retrieved plants match the test data
-	if len(plants) != len(testPlants) {
-		t.Errorf("Expected %d plants, but got %d", len(testPlants), len(plants))
-	}
-	for i := range testPlants {
-		if plants[i].CommonName != testPlants[i].CommonName || plants[i].ScientificName != testPlants[i].ScientificName || plants[i].Genus != testPlants[i].Genus || plants[i].Rank != testPlants[i].Rank {
-			t.Errorf("Expected plant %+v, but got %+v", testPlants[i], plants[i])
-		}
+	// Check that the plant was updated correctly
+	updatedPlant, found := Garden[plantName]
+	if !found {
+		t.Errorf("Plant %q was not found in the garden", plantName)
+	} else if updatedPlant.Description != "A beautiful and fragrant flower." {
+		t.Errorf("Plant %q description was not updated correctly. Got %q; expected %q", plantName, updatedPlant.Description, "A beautiful and fragrant flower.")
 	}
 }
