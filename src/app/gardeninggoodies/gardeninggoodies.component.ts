4f96dae8edfd799da5e5d6a13446e92777202b2a
naserash <imnotmaxed@gmail.com>
2023-03-29 10:38:00 -0400

    Amazon Ads for Gardening Goodies Page

--- src/app/gardeninggoodies/gardeninggoodies.component.ts
+++ src/app/gardeninggoodies/gardeninggoodies.component.ts
@@ -3,13 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gardeninggoodies',
   template: `
-    <p>
-      gardeninggoodies works!
-    </p>
+  <h2 class="title is-2 is-capitalized">Gardening Tools</h2>
+    <div class="iframe-wrapper">
+    
+      <iframe #amazonAd src="assets/amazon-ad.html" width="100%" height="600" frameborder="0" (load)="onIframeLoad(amazonAd)"></iframe>
+    </div>
   `,
   styles: [
-  ]
+    `
+      .iframe-wrapper {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 56.25%; /* 16:9 Aspect Ratio */
+        overflow: hidden;
+      }
+
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border: none;
+      }
+    `,
+  ],
 })
 export class GardeninggoodiesComponent {
+  onIframeLoad(iframe: HTMLIFrameElement) {
+    const iframeWindow = iframe.contentWindow;
 
+    if (iframeWindow) {
+      iframeWindow.postMessage({ type: 'init' }, '*');
+      iframeWindow.addEventListener('message', (event) => {
+        if (event.data.type === 'resize') {
+          iframe.style.height = event.data.height + 'px';
+        }
+      });
+    }
+  }
 }
