0e6d0836526430ac87d5ed9f1fb4ad2d0c419097
shaliniApatel03 <patels3@ufl.edu>
2023-04-15 11:52:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 import re
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import openai
 
 app = Flask(__name__)
 
-openai.api_key = "openai_api-key"
+openai.api_key = "OPENAI_API_KEY"
 model = "text-davinci-002"  # change the model as needed
 
-def get_plant_details(plant_name):
-    prompt = f"Generate details for the plant {plant_name}:\n\nScientific name:\nFlower type:\nSeed types:\nSeason type:\nPreferred soil type:\nPreferred pH level:\nPreferred nutrition:\nHarvest time:\nPlant compatibility:"
-    model = "text-davinci-002" # change the model as needed
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=2048,
-        n=1,
-        stop=None,
-        temperature=0.7,
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        plant = request.form["plant"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(plant),
+            temperature=0.6,
+            max_tokens=250,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(plant):
+    return """Provide the following one line details for the given plant.
+    
+    Plant name: Cucumber\n
+    Plant name: Cucumber\n
+    Scientific name: Cucumis sativus\n
+    Flower type: Yellow\n
+    Seed types: Monoecious or dioecious, depending on the cultivar\n
+    Season type: Warm season crop\n
+    Preferred soil type: Well-drained, fertile soil\n
+    Preferred pH level: 6.0-7.0\n
+    Preferred nutrition: Balanced fertilizer with more emphasis on nitrogen\n
+    Harvest time: 50-70 days after sowing\n
+    Plant compatibility: Avoid planting with aromatic herbs or nightshades.\n
+    Plant: {}""".format(
+        plant.capitalize()
     )
 
-    if response.choices[0].text:
-        return response.choices[0].text
-    else:
-        return "Plant not found."
 
-
-
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        plant_name = request.form.get("plant_name")
-        plant_details = get_plant_details(plant_name)
-        return render_template("results.html", plant_name=plant_name, plant_details=plant_details)
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
